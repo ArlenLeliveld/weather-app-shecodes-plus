ecc59904c9c4e25543978608dc05c522c9acfd47
ArlenLeliveld <arlen.leliveld@outlook.com>
2023-05-09 00:42:46 +0200

    Removed conversion to fahrenheit

--- src/app.js
+++ src/app.js
@@ -34,7 +34,6 @@ function formatDay(timestamp) {
   return days[day];
 }
 function displayForecast(response) {
-  console.log(response.data.daily);
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
@@ -70,23 +69,17 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "caa883a4a60d93878755b08a933f74ea";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios(apiUrl).then(displayForecast);
 }
 function displayTemperature(response) {
-  console.log(response.data);
   let currentTemp = document.querySelector("#degrees");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
   let condition = document.querySelector("#description");
   let icon = document.querySelector("#icon");
-  celsiusTemperature = response.data.main.temp;
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  currentTemp.innerHTML = Math.round(celsiusTemperature);
+  currentTemp.innerHTML = Math.round(response.data.main.temp);
   humidity.innerHTML = Math.round(response.data.main.humidity);
   wind.innerHTML = Math.round(response.data.wind.speed);
   condition.innerHTML = response.data.weather[0].description;
@@ -114,9 +107,7 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleInput);
 
 function here(response) {
-  console.log(response.data);
   let currentPlace = response.data.name;
-  console.log(currentPlace);
   let h1 = document.querySelector("h1");
   h1.innerHTML = currentPlace;
   let currentTemp = document.querySelector("#degrees");
@@ -131,9 +122,7 @@ function here(response) {
   icon.setAttribute("alt", "icon");
 }
 function showPosition(position) {
-  console.log(position.coords.latitude);
   let lat = position.coords.latitude;
-  console.log(position.coords.longitude);
   let lon = position.coords.longitude;
   let apiKey = "caa883a4a60d93878755b08a933f74ea";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
@@ -149,30 +138,5 @@ function currentLocation(event) {
 }
 let currentLoc = document.querySelector("#currentLocationButton");
 currentLoc.addEventListener("click", currentLocation);
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  let degrees = document.querySelector("#degrees");
-  degrees.innerHTML = Math.round(fahrenheitTemperature);
-  celsiusLink.classList.remove("active");
-  celsiusLink.classList.add("inactive");
-  fahrenheitLink.classList.add("active");
-}
-let fahrenheitLink = document.querySelector("#fahrenheitLink");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  let degrees = document.querySelector("#degrees");
-  degrees.innerHTML = Math.round(celsiusTemperature);
-  fahrenheitLink.classList.remove("active");
-  fahrenheitLink.classList.add("inactive");
-  celsiusLink.classList.add("active");
-}
-let celsiusLink = document.querySelector("#celsiusLink");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-let celsiusTemperature = null;
 
 search("Koksijde");
