16e48859b8c5f8ac78016d60f96c4026e3cd1990
ArlenLeliveld <arlen.leliveld@outlook.com>
2023-05-08 21:17:08 +0200

    implemented API

--- src/app.js
+++ src/app.js
@@ -19,7 +19,9 @@ if (minutes < 10) {
   minutes = `0${minutes}`;
 }
 today.innerHTML = `${day} ${hours}:${minutes}`;
-function displayForecast() {
+
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"];
   let forecastHTML = `<div class="row">`;
@@ -43,8 +45,15 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "caa883a4a60d93878755b08a933f74ea";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
-  console.log(response.data);
+  console.log(response.data.daily);
   let currentTemp = document.querySelector("#degrees");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
@@ -62,6 +71,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", "icon");
+
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "e96362f7a3e49741bc7f62bcb18d316c";
@@ -100,7 +111,7 @@ function showPosition(position) {
   let lat = position.coords.latitude;
   console.log(position.coords.longitude);
   let lon = position.coords.longitude;
-  let apiKey = "e96362f7a3e49741bc7f62bcb18d316c";
+  let apiKey = "caa883a4a60d93878755b08a933f74ea";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(here);
@@ -136,4 +147,3 @@ let celsiusLink = document.querySelector("#celsiusLink");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let celsiusTemperature = null;
-displayForecast();
